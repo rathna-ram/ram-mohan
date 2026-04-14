8204fd18ff743b9aee18064e8d3ff8224f65a3ad
Rathna kumar <laddumudhiraj35@gmail.com>
2026-04-14 08:56:43 +0530

    chore: prepare for production deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-new-project1/'   // 👈 VERY IMPORTANT (repo name)
+  base: '/my-project/'   // 👈 VERY IMPORTANT (repo name)
 })
